f153cf6558d10b5515413a42a2a15a00e55f8670
Menno de Haas <menno.dehaas@webflight.nl>
2017-06-23 12:05:17 +0200

    New Java action CreateLogNode
    Java action initialized LogNode. As a result, the log level can be configured without having log.

--- CommunityCommons_src/javasource/system/UserActionsRegistrar.java
+++ CommunityCommons_src/javasource/system/UserActionsRegistrar.java
@@ -47,6 +47,7 @@ public class UserActionsRegistrar implements EventHandler
 			component.actionRegistry().registerUserAction(communitycommons.actions.commitInSeparateDatabaseTransaction.class);
 			component.actionRegistry().registerUserAction(communitycommons.actions.commitWithoutEvents.class);
 			component.actionRegistry().registerUserAction(communitycommons.actions.copyAttributes.class);
+			component.actionRegistry().registerUserAction(communitycommons.actions.CreateLogNode.class);
 			component.actionRegistry().registerUserAction(communitycommons.actions.DateTimeToLong.class);
 			component.actionRegistry().registerUserAction(communitycommons.actions.DecryptString.class);
 			component.actionRegistry().registerUserAction(communitycommons.actions.DeepClone.class);
@@ -114,6 +115,10 @@ public class UserActionsRegistrar implements EventHandler
 			component.actionRegistry().registerUserAction(communitycommons.actions.StringTrim.class);
 			component.actionRegistry().registerUserAction(communitycommons.actions.SubstituteTemplate.class);
 			component.actionRegistry().registerUserAction(communitycommons.actions.SubstituteTemplate2.class);
+			component.actionRegistry().registerUserAction(communitycommons.actions.SubstringAfter.class);
+			component.actionRegistry().registerUserAction(communitycommons.actions.SubstringAfterLast.class);
+			component.actionRegistry().registerUserAction(communitycommons.actions.SubstringBefore.class);
+			component.actionRegistry().registerUserAction(communitycommons.actions.SubstringBeforeLast.class);
 			component.actionRegistry().registerUserAction(communitycommons.actions.ThrowException.class);
 			component.actionRegistry().registerUserAction(communitycommons.actions.ThrowWebserviceException.class);
 			component.actionRegistry().registerUserAction(communitycommons.actions.TimeMeasureEnd.class);
