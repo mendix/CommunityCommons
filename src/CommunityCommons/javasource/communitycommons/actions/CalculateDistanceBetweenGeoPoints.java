086c659e41af76acb91d2edaf787a72bc1e5d7e4
Jaap van Leeuwen <73707148+JaapvanLeeuwen@users.noreply.github.com>
2021-03-05 10:49:10 +0100

    Commit review feedback Menno
    Implemented Menno's feedback, put calculating action in separate class.

--- src/CommunityCommons/javasource/communitycommons/actions/CalculateDistanceBetweenGeoPoints.java
+++ src/CommunityCommons/javasource/communitycommons/actions/CalculateDistanceBetweenGeoPoints.java
@@ -47,7 +47,11 @@ public class CalculateDistanceBetweenGeoPoints extends CustomJavaAction<java.mat
 		double lng2 = LongitudeValueToCompare.doubleValue();
 		double earthRadius = EarthRadius.doubleValue();
 		
-		return calculateDistance(lat1, lng1, lat2, lng2, earthRadius);
+		DistanceBetweenGeoPointsCalculator calculatedDistance = new DistanceBetweenGeoPointsCalculator();
+		BigDecimal distance = calculatedDistance.calculateDistance(lat1, lng1, lat2, lng2, earthRadius);
+		
+
+		return distance;
 		
 		// END USER CODE
 	}
@@ -62,18 +66,5 @@ public class CalculateDistanceBetweenGeoPoints extends CustomJavaAction<java.mat
 	}
 
 	// BEGIN EXTRA CODE
-	
-	private BigDecimal calculateDistance(double lat1, double lng1, double lat2, double lng2, double earthRadius) {	
-		double dLat = Math.toRadians(lat2-lat1);
-		double dLng = Math.toRadians(lng2-lng1);
-		double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-	        Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-	        Math.sin(dLng/2) * Math.sin(dLng/2);
-		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-		BigDecimal dist = BigDecimal.valueOf(earthRadius * c);
-
-		return dist;
-	}
-	
 	// END EXTRA CODE
 }
